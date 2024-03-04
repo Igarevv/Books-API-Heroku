33acf540e37c3299c0f4e328dc4aea56a154c43c
Ihor <120051094+Igarevv@users.noreply.github.com>
2024-03-04 19:05:39 +0200

    Update database.php

--- configs/database.php
+++ configs/database.php
@@ -3,7 +3,7 @@
 return [
   'driver'   => 'mysql',
   'dbname'   => 'BookAPI',
-  'host'     => 'localhost', // mysql for docker, localhost for local
+  'host'     => 'mysql', // mysql for docker, localhost for local
   'user'     => 'dev',
   'password' => 'Devbase75_',
 ];
