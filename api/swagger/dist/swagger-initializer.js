c5d53ade04a3e9bb5985bc8c3f0d0c19160b27cb
Igarevv <forstudy757@gmail.com>
2024-05-04 23:33:05 +0300

    docker for render host

--- api/swagger/dist/swagger-initializer.js
+++ api/swagger/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:8000/public/swagger/dist/swagger.json",
+    url: "https://books-gxzylgunz-igarevvs-projects.vercel.app/api/swagger/dist/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
