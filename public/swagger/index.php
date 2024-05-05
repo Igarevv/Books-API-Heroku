82e22f746f0e9ca22492c9a0194c5ed74bc4b72b
Igarevv <forstudy757@gmail.com>
2024-05-05 12:18:41 +0300

    vercel

--- public/swagger/index.php
+++ public/swagger/index.php
@@ -3,6 +3,6 @@ define('APP_PATH', dirname(__DIR__, 2));
 
 header("Content-Type: text/html");
 
-require APP_PATH.'/api/swagger/dist/index.html';
+require APP_PATH.'/public/swagger/dist/index.html';
 
 exit;
