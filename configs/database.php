93ab9910735ff17e13c9f6a8d0c77414201de897
Igarevv <forstudy757@gmail.com>
2024-05-05 11:46:35 +0300

    vercel

--- configs/database.php
+++ configs/database.php
@@ -1,9 +1,17 @@
 <?php
 
-return [
+/*return [
   'driver'   => 'mysql',
   'dbname'   => 'BookAPI',
   'host'     => 'mysql', // mysql for docker, localhost for local
   'user'     => 'dev',
   'password' => 'Devbase75_',
+];*/
+
+return [
+  'driver'   => 'mysql',
+  'dbname'   => 'dd5fa6l2aac8ac',
+  'host'     => 'cf9gid2f6uallg.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com', // mysql for docker, localhost for local
+  'user'     => 'u9120be039bei0',
+  'password' => 'p763582d1d9e2ce1cbf2806dfdcd338dfe73b83c5384a0a3beb1b8839a2d9870b',
 ];
