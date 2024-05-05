2f8f94dba9e3fdfe888c13c2665bd03c58910e3d
Igarevv <forstudy757@gmail.com>
2024-05-05 12:52:55 +0300

    vercel

--- public/index.php
+++ public/index.php
@@ -2,11 +2,14 @@
 error_reporting(0);
 use App\App;
 use Symfony\Component\Dotenv\Dotenv;
-header("Access-Control-Allow-Origin: https://sleepy-everglades-57627-a28a0b484e07.herokuapp.com");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Forwarded-With");
 header("Access-Control-Allow-Methods: GET, POST, DELETE");
 
 if ($_SERVER['REQUEST_URI'] === '/swagger'){
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Forwarded-With");
+    header("Access-Control-Allow-Methods: GET, POST, DELETE");
     require APP_PATH . '/public/swagger/index.php';
     exit();
 }
