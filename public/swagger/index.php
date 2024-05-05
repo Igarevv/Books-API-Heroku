8cc4cf7a557fa7ce882884010b8a8d1d5ed741c6
Igarevv <forstudy757@gmail.com>
2024-05-05 12:42:20 +0300

    vercel

--- public/swagger/index.php
+++ public/swagger/index.php
@@ -1,9 +1,10 @@
 <?php
 define('APP_PATH', dirname(__DIR__, 2));
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Forwarded-With");
-header("Access-Control-Allow-Methods: GET, POST, DELETE");
-header("Content-Type: text/html");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json");
 
 require APP_PATH.'/public/swagger/dist/index.html';
 
