3b574fe8c3e7a77ab02b898772ba41b4ff120395
Igarevv <forstudy757@gmail.com>
2024-05-06 14:55:14 +0300

    heroku

--- configs/database.php
+++ configs/database.php
@@ -10,7 +10,7 @@
 
 return [
   'driver'   => 'pgsql',
-  'dbname'   => 'dd5fa6l2aac8ac',
+  'dbname'   => 'dccbia0l7bl0i6',
   'host'     => 'c7vbm80blivm58.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com', // mysql for docker, localhost for local
   'user'     => 'u3ie1bpsuob73q',
   'password' => 'pb33ac2648631cb865e8f21145b03bf486fa3e7c786ef967cea366d2f1312f66c',
