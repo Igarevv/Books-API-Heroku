4b426248c7b44d13785c0ae13fb3a24cc4e8dcd9
Igarevv <forstudy757@gmail.com>
2024-05-06 15:09:58 +0300

    heroku]

--- configs/database.php
+++ configs/database.php
@@ -8,10 +8,11 @@
   'password' => 'Devbase75_',
 ];*/
 
+$db = parse_url(getenv("DATABASE_URL"));
+
 return [
   'driver'   => 'pgsql',
-  'dbname'   => 'dccbia0l7bl0i6',
-  'host'     => 'c7vbm80blivm58.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com', // mysql for docker, localhost for local
-  'user'     => 'u3ie1bpsuob73q',
-  'password' => 'pb33ac2648631cb865e8f21145b03bf486fa3e7c786ef967cea366d2f1312f66c',
+  'host'     => $db['host'],
+  'user'     => $db['user'],
+  'password' => $db['pass'],
 ];
